c4999b3384ac7dffed92d719571f39281ead8e8a
Better Futures <betterfuture032@gmail.com>
2022-12-04 00:14:55 +0100

    final commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  ignoreBuildErrors: true,
   swcMinify: true,
   images: {
     domains: ["lh3.googleusercontent.com", "cdn.sanity.io"],
--- utils/getStripe.ts
+++ utils/getStripe.ts
@@ -1,6 +1,6 @@
-import { loadStripe} from "@stripe/stripe-js";
+import { loadStripe, Stripe} from "@stripe/stripe-js";
 
-let stripePromise;
+let stripePromise: Promise<Stripe | null>;
 
 const getStripe = () => {
     if(!stripePromise){
