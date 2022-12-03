d59bde5e946f7e6174eff36c74d3438f11d10c21
Better Futures <betterfuture032@gmail.com>
2022-12-04 00:17:21 +0100

    final commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  ignoreBuildErrors: true,
+  typescript:{
+    ignoreBuildErrors: true 
+  },
   swcMinify: true,
   images: {
     domains: ["lh3.googleusercontent.com", "cdn.sanity.io"],
